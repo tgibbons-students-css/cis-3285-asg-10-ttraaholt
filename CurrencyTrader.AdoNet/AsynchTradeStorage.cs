2fcb1d0c559061b0cad9f7764861441a7090afbf
ttraaholt <ttraaholt@css.edu>
2016-11-10 11:42:17 -0600

    added AsynchTradeStorage

---
+++ CurrencyTrader.AdoNet/AsynchTradeStorage.cs
@@ -0,0 +1,18 @@
+﻿using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrader.AdoNet
+{
+    class AsynchTradeStorage : ITradeStorage
+    {
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
